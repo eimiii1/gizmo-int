d993e01a78bc205b66201ddf488ba45f4fab7d7f
eimiii1 <philipjohn1627@gmail.com>
2026-09-12 15:19:16 +0800

    initial: setup database (mysql)

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,23 @@
-from flask import Flask 
+from flask import Flask , jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    
+    db.init_app(app)
+
+    @app.route('/ping')
+    def ping():
+        return jsonify({'message' : 'pong'})
     
     return app
