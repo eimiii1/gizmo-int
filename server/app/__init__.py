74c7ac12722b01a9c1ab9b718c7fa4333d3e4563
eimiii1 <philipjohn1627@gmail.com>
2026-09-12 17:31:03 +0800

    feat: register auth blueprint

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy 
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+from app.routes.auth import auth_bp
 from dotenv import load_dotenv
 import os
 
@@ -21,6 +22,8 @@ def create_app():
     db.init_app(app)
     jwt.init_app(app)
     bcrypt.init_app(app)
+    
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     @app.route('/ping')
     def ping():
