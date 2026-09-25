b04054d8feae1256261fde75b8cfae415bda24e1
Philip Barcelo <philip@Philips-iMac.local>
2026-09-25 23:16:06 +0800

    feat: register the auth blueprint (auth_bp) in app.py to initialize it

--- server/run.py
+++ server/run.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from config import Config
+from models import db
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db.init_app(app)
+app.register_blueprint(auth_bp)
 
 @app.route('/')
 def home():
     return "Hello nig"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
