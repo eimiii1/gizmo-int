e0fe734a63614ae1d129971f76d6ac8853ba9fb0
eimiii1 <philipjohn1627@gmail.com>
2026-09-26 22:29:49 +0800

    feat: add router dom

--- test-client/src/main.jsx
+++ test-client/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Register from './pages/register.jsx'
+import Login from './pages/login.jsx'
+import Me from './pages/me.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/me' element={<Me />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
