058bb34836de2468107700a9635d5b09de6d147f
eimiii1 <philipjohn1627@gmail.com>
2026-09-26 18:34:27 +0800

    feat: add a protected route for the logged in user

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from models import db, User
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api')
 
@@ -53,4 +53,19 @@ def login():
             "username" : user.username,
             "email" : user.email
         }
+    }), 200
+
+@auth_bp.route('/me', methods=["GET"])
+@jwt_required()
+def me():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({'error' : 'User not found!'}), 404
+
+    return jsonify({
+        'id' : user.id,
+        'username' : user.username,
+        'email' : user.email
     }), 200
