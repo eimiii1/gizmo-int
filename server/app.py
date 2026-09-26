66bacca7036c7b27b39c435efa2022a0595735e0
eimiii1 <philipjohn1627@gmail.com>
2026-09-26 21:33:35 +0800

    feat: initialize the cors in app.py | should it be your localhost or 127.0.0.1 and your port depends on which you set it to

--- server/app.py
+++ server/app.py
@@ -3,9 +3,12 @@ from config import Config
 from models import db
 from routes.auth import auth_bp
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+CORS(app, origins=["http://localhost:5173", "http://127.0.0.1:5173"])
 
 db.init_app(app)
 app.register_blueprint(auth_bp)
