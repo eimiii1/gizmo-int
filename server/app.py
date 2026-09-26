ba9bd5132d1f3fc04087441a546fbb466358ba57
Philip Barcelo <philip@Philips-iMac.local>
2026-09-26 17:44:43 +0800

    refactor: rename app file

--- server/app.py
+++ server/app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from config import Config
 from models import db
 from routes.auth import auth_bp
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
 app.register_blueprint(auth_bp)
+
+jwt = JWTManager(app)
 
 @app.route('/')
 def home():
